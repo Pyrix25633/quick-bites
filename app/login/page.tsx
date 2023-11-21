b2f548bfe7844ffa022899e8e99e831ce56770b1
Pyrix25633 <biral.mattia@gmail.com>
2023-11-21 12:58:00 +0100

    Testing Next.js functionalities and installed mui

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+import { Box, Card, Container, Typography } from "@mui/material"
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Login page"
+}
+
+export default function Login() {
+    return (
+        <main>
+            <Container>
+                <Box>
+                    <Card>
+                        <Typography variant="h3">Test</Typography>
+                    </Card>
+                    <Card>
+                        <Typography variant="h3">Test</Typography>
+                    </Card>
+                </Box>
+            </Container>
+        </main>
+        
+    )
+}
