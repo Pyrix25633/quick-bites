2564134c48264514026e6304deee21fb3c103d80
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-01-03 19:43:37 +0100

    Remove MUI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,25 +1,11 @@
-import { Box, Card, Container, Typography } from "@mui/material"
-import { Metadata } from "next"
-
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Login",
     description: "Login page"
-}
+};
 
 export default function Login() {
-    return (
-        <main>
-            <Container>
-                <Box>
-                    <Card>
-                        <Typography variant="h3">Test</Typography>
-                    </Card>
-                    <Card>
-                        <Typography variant="h3">Test</Typography>
-                    </Card>
-                </Box>
-            </Container>
-        </main>
-    )
+    return <main></main>;
 }
+
