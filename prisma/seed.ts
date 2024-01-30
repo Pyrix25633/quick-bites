62b99026b122b7d8a4ab72fc28b300e3fbd08a97
1mihai0 <mihai.bostan@iisvittorioveneto.it>
2024-01-30 13:00:36 +0100

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,11 +11,19 @@ async function main() {
         }
     });
 
-    await prisma.school.upsert({
+    const ite = await prisma.school.upsert({
         where: { name: "ITE Vittorio Veneto" },
         update: {},
         create: {
             name: "ITE Vittorio Veneto"
+        }
+    });
+
+    const ipsia = await prisma.school.upsert({
+        where: { name: "IPSIA Vittorio Veneto" },
+        update: {},
+        create: {
+            name: "IPSIA Vittorio Veneto"
         }
     });
 
@@ -47,6 +55,86 @@ async function main() {
             credit: new Prisma.Decimal(50.2),
             school: {
                 connect: itt
+            }
+        }
+    });
+
+    const tony = await prisma.user.upsert({
+        where: { username: "tony.fassina" },
+        update: {},
+        create: {
+            username: "tony.fassina",
+            passwordHash:
+                "$2a$12$77GyV3xyf8.Rq/W7qdWJs.ReFSN9mDtJXecxpO8GchEfGdtj609cL",
+            email: "tony.fassina@example.com",
+            role: "BUYER",
+            credit: new Prisma.Decimal(20.4),
+            school: {
+                connect: ipsia
+            }
+        }
+    });
+
+    const silvio = await prisma.user.upsert({
+        where: { username: "silvio.denardi" },
+        update: {},
+        create: {
+            username: "silvio.denardi",
+            passwordHash:
+                "$2a$12$77GyV3xyf8.Rq/W7qdWJs.ReFSN9mDtJXecxpO8GchEfGdtj610tY",
+            email: "silvio.denardi@example.com",
+            role: "BUYER",
+            credit: new Prisma.Decimal(69.4),
+            school: {
+                connect: ite
+            }
+        }
+    });
+
+    const cincia = await prisma.user.upsert({
+        where: { username: "cincia.fosforo" },
+        update: {},
+        create: {
+            username: "cincia.fosforo",
+            passwordHash:
+                "$2a$12$77GyV3xyf8.Rq/W7qdWJs.ReFSN9mDtJXecxpO8GchEfGdtj678bO",
+            email: "cincia.fosforo@example.com",
+            role: "BUYER",
+            credit: new Prisma.Decimal(10.4),
+            school: {
+                connect: ite
+            }
+        }
+    });
+
+    const simon = await prisma.user.upsert({
+        where: { username: "simon.tacastaca" },
+        update: {},
+        create: {
+            username: "simon.tacastaca",
+            passwordHash:
+                "$2a$12$77GyV3xyf8.Rq/W7qdWJs.ReFSN9mDtJXecxpO8GchEfGdtj645rT",
+            email: "simon.tacastaca@example.com",
+            role: "BUYER",
+            credit: new Prisma.Decimal(190.4),
+            school: {
+                connect: ipsia
+            }
+        }
+    });
+
+    const francesco = await prisma.user.upsert({
+        where: { username: "francesco.basso" },
+        update: {},
+        create: {
+            username: "francesco.basso",
+            passwordHash:
+                "$2a$12$77GyV3xyf8.Rq/W7qdWJs.ReFSN9mDtJXecxpO8GchEfGdtj656eW",
+            email: "francesco.basso@example.com",
+            role: "BUYER",
+            credit: new Prisma.Decimal(80.3),
+            school: {
+                connect: ipsia 
             }
         }
     });
