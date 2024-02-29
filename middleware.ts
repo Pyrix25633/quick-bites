7eb82e8f1d0e44b19d0ab0dd3c29d9cba635e645
Pyrix25633 <biral.mattia@gmail.com>
2024-02-29 08:25:55 +0100

    Modified localization middleware and added language setting for users

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { withLocalization } from "./middlewares/withLocalization";
 
 acceptLanguage.languages(languages);
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"]
+    matcher: ["/((?!_next/static|_next/image|assets|favicon.ico|sw.js).*)"]
 };
 
 export function middleware(request: NextRequest): NextResponse {
--- middlewares/withLocalization.ts
+++ middlewares/withLocalization.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import acceptLanguage from "accept-language";
 import { fallbackLanguage, languages, cookieName } from "../i18n/settings";
 
-export function withLocalization(request: NextRequest) {
+export function withLocalization(request: NextRequest): void {
+    if (request.nextUrl.pathname.match(/\/api\//)) return;
+
     let language;
     const cookie = request.cookies.get(cookieName);
     if (cookie != undefined) language = acceptLanguage.get(cookie.value);
@@ -26,17 +28,5 @@ export function withLocalization(request: NextRequest) {
                 request.url
             )
         );
-    }
-
-    if (request.headers.has("referer")) {
-        const referer = request.headers.get("referer");
-        const refererUrl = new URL(referer ?? "");
-        const languageInReferer = languages.find((l) =>
-            refererUrl.pathname.startsWith(`/${l}`)
-        );
-        const response = NextResponse.next();
-        if (languageInReferer)
-            response.cookies.set(cookieName, languageInReferer);
-        throw response;
     }
 }
