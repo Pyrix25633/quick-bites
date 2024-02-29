3244b16edd02c7bb89f5c8306fc88a0d6a3a4513
Lorenzo Ianotto <lorenzus.ian8@gmail.com>
2024-02-29 12:01:27 +0100

    Add login page functionality

---
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import React, { ForwardedRef, useState } from "react";
+import { InputHTMLAttributes } from "react";
+import { GetRequestBody } from "@/app/api/auth/login/route";
+import { useRouter } from "next/navigation";
+
+//  TODO: add errors to text inputs (red circle and description underneath)
+type FormValues = GetRequestBody;
+
+function LoginForm() {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit
+        // formState: { errors }
+    } = useForm<FormValues>();
+
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    const onSubmit = (data: FormValues) => {
+        axios
+            .post("/api/auth/login", data)
+            .then((_) => router.push("/"))
+            .catch((err) => {
+                if (err?.response?.status) {
+                    switch (err.response.status) {
+                        case 404:
+                            setErrorMessage("Utente non esistente");
+                            break;
+                        case 401:
+                            setErrorMessage("Password errata");
+                            break;
+                        default:
+                            setErrorMessage("Errore di comunicazione");
+                    }
+                } else {
+                    setErrorMessage("Errore di rete");
+                }
+            });
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex w-full max-w-sm flex-col items-stretch gap-4 rounded-2xl bg-secondary-light p-10"
+        >
+            <TextInput
+                label="Username"
+                type="text"
+                {...register("username", { required: true })}
+            />
+            <TextInput
+                label="Password"
+                type="password"
+                {...register("password", { required: true })}
+            />
+            <div className="text-red-600">{errorMessage}</div>
+            <button
+                type="submit"
+                className="self-center rounded-2xl bg-primary px-4 py-2 text-2xl text-secondary-light"
+            >
+                Entra
+            </button>
+        </form>
+    );
+}
+
+type TextInputProps = {
+    label?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const TextInput = React.forwardRef(function TextInput(
+    { label, ...props }: TextInputProps,
+    ref: ForwardedRef<HTMLInputElement>
+) {
+    return (
+        <div>
+            {label && (
+                <label htmlFor={props.name} className="text-2xl text-primary">
+                    {label}
+                </label>
+            )}
+            <input
+                id={props.name}
+                className="w-full rounded-lg border-0 bg-secondary-dark text-primary focus:ring-primary"
+                ref={ref}
+                {...props}
+            />
+        </div>
+    );
+});
+
+export default LoginForm;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Image from "next/image";
 import logo from "@/public/images/logo.svg";
+import LoginForm from "./_components/LoginForm";
 
 export const metadata: Metadata = {
     title: "Login",
@@ -11,35 +12,7 @@ export default function Login() {
     return (
         <main className="container flex h-screen flex-col items-center">
             <Image alt="QuickBites logo" src={logo} className="static h-1/3" />
-            <form className="flex w-full max-w-sm flex-col items-stretch gap-4 rounded-2xl bg-secondary-light p-10">
-                <TextInput label="Email" name="email" type="email" />
-                <TextInput label="Password" name="password" type="password" />
-                <button
-                    type="submit"
-                    className="self-center rounded-2xl bg-primary px-4 py-2 text-2xl text-secondary-light"
-                >
-                    Entra
-                </button>
-            </form>
+            <LoginForm />
         </main>
-    );
-}
-
-type TextInputProps = {
-    label?: string;
-    name: string;
-    type?: "text" | "email" | "password" | "number";
-};
-
-function TextInput({ label, name, type = "text" }: TextInputProps) {
-    return (
-        <label>
-            {label && <div className="text-2xl text-primary">{label}</div>}
-            <input
-                name={name}
-                type={type}
-                className="w-full rounded-lg border-0 bg-secondary-dark text-primary focus:ring-primary"
-            />
-        </label>
     );
 }
